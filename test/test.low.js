1a55ec4e2114b111dace3c4b6441ef01cfccb91d
kgryte <kgryte@gmail.com>
2016-01-18 05:33:16 -0800

    Add files

---
+++ test/test.low.js
@@ -0,0 +1,34 @@
+'use strict';
+
+// MODULES //
+
+var tape = require( 'tape' );
+var proxyquire = require( 'proxyquire' );
+var LOW = require( './../lib/low.js' );
+
+
+// TESTS //
+
+tape( 'main export is a number', function test( t ) {
+	t.ok( typeof LOW === 'number', 'main export is a number' );
+	t.end();
+});
+
+tape( 'if little endian, the export equals 0', function test( t ) {
+	var LOW = proxyquire( './../lib/low.js', {
+		'utils-is-little-endian': true
+	});
+
+	t.equal( LOW, 0, 'LOW equals 0' );
+	t.end();
+});
+
+tape( 'if big endian, the export equals 1', function test( t ) {
+	var LOW = proxyquire( './../lib/low.js', {
+		'utils-is-little-endian': false
+	});
+
+	t.equal( LOW, 1, 'LOW equals 1' );
+	t.end();
+});
+
